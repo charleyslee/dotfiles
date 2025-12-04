e43f3785e9772dfa0a17342e91ddd11be1b3673b
Charley <charleylee42@gmail.com>
2025-12-04 02:58:09 -0800

    update

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,32 +1,34 @@
 return {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   deps = {
     "nvim-lua/plenary.nvim",
   },
   config = function()
-    local mark = require("harpoon.mark")
-    local ui = require("harpoon.ui")
+    local harpoon = require("harpoon")
 
-    vim.keymap.set("n", "<leader>m", mark.add_file)
-    vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
+    harpoon:setup()
+
+    vim.keymap.set("n", "<leader>m", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
     vim.keymap.set("n", "<leader>1", function()
-      ui.nav_file(1)
+      harpoon:list():select(1)
     end)
     vim.keymap.set("n", "<leader>2", function()
-      ui.nav_file(2)
+      harpoon:list():select(2)
     end)
     vim.keymap.set("n", "<leader>3", function()
-      ui.nav_file(3)
+      harpoon:list():select(3)
     end)
     vim.keymap.set("n", "<leader>4", function()
-      ui.nav_file(4)
+      harpoon:list():select(4)
     end)
     vim.keymap.set("n", "<leader>5", function()
-      ui.nav_file(5)
+      harpoon:list():select(5)
     end)
     vim.keymap.set("n", "<leader>6", function()
-      ui.nav_file(6)
+      harpoon:list():select(6)
     end)
   end,
 }
